01174c61fa5dd75437ec86ba0a79effa2fcd0723
samatstarion <s.gerene@stariongroup.eu>
2025-03-13 11:18:06 +0100

    [Add] expected serialilzers and deserializers

---
+++ KerML.NET.CodeGenerator.Tests/Expected/UML/AutoGenSerializer/AnnotationSerializer.cs
@@ -0,0 +1,167 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="AnnotationSerializer.cs" company="Starion Group S.A.">
+//
+//   Copyright 2022-2025 Starion Group S.A.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
+
+namespace KerML.NET.Serializer.Json
+{
+    using System;
+    using System.Text.Json;
+
+    using SysML2.NET.Common;
+    using SysML2.NET.Core.DTO;
+    using SysML2.NET.Serializer.Json;
+
+    /// <summary>
+    /// The purpose of the <see cref="AnnotationSerializer"/> is to provide serialization capabilities
+    /// capabilities for the <see cref="IAnnotation"/> interface
+    /// </summary>
+    public static class AnnotationSerializer
+    {
+        /// <summary>
+        /// Serializes an instance of <see cref="IAnnotation"/> using an <see cref="Utf8JsonWriter"/>
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="IAnnotation"/> to serialize
+        /// </param>
+        /// <param name="writer">
+        /// The target <see cref="Utf8JsonWriter"/>
+        /// </param>
+        /// <param name="serializationModeKind">
+        /// enumeration specifying what kind of serialization shall be used
+        /// </param>
+        /// <param name="inludeDerived">
+        /// A value indicating whether derived properties shall be serialized or not
+        /// </param>
+        public static void Serialize(object obj, Utf8JsonWriter writer, SerializationModeKind serializationModeKind, bool inludeDerived)
+        {
+            if (!(obj is IAnnotation iAnnotation))
+            {
+                throw new ArgumentException("The object shall be an IAnnotation", nameof(obj));
+            }
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("@type"u8);
+            writer.WriteStringValue("Annotation"u8);
+
+            writer.WritePropertyName("@id"u8);
+            writer.WriteStringValue(iAnnotation.Id);
+
+            writer.WriteStartArray("aliasIds"u8);
+            foreach (var item in iAnnotation.AliasIds)
+            {
+                writer.WriteStringValue(item);
+            }
+            writer.WriteEndArray();
+
+            writer.WritePropertyName("annotatedElement"u8);
+            writer.WriteStartObject();
+            writer.WritePropertyName("@id"u8);
+            writer.WriteStringValue(iAnnotation.AnnotatedElement);
+            writer.WriteEndObject();
+
+            writer.WritePropertyName("declaredName"u8);
+            writer.WriteStringValue(iAnnotation.DeclaredName);
+            writer.WritePropertyName("declaredShortName"u8);
+            writer.WriteStringValue(iAnnotation.DeclaredShortName);
+            writer.WritePropertyName("elementId"u8);
+            writer.WriteStringValue(iAnnotation.ElementId);
+
+            writer.WritePropertyName("isImplied"u8);
+            writer.WriteBooleanValue(iAnnotation.IsImplied);
+
+            writer.WritePropertyName("isImpliedIncluded"u8);
+            writer.WriteBooleanValue(iAnnotation.IsImpliedIncluded);
+
+            writer.WriteStartArray("ownedRelatedElement"u8);
+            foreach (var item in iAnnotation.OwnedRelatedElement)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("@id"u8);
+                writer.WriteStringValue(item);
+                writer.WriteEndObject();
+            }
+            writer.WriteEndArray();
+
+            writer.WriteStartArray("ownedRelationship"u8);
+            foreach (var item in iAnnotation.OwnedRelationship)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("@id"u8);
+                writer.WriteStringValue(item);
+                writer.WriteEndObject();
+            }
+            writer.WriteEndArray();
+
+            writer.WritePropertyName("owningRelatedElement"u8);
+            if (iAnnotation.OwningRelatedElement.HasValue)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("@id"u8);
+                writer.WriteStringValue(iAnnotation.OwningRelatedElement.Value);
+                writer.WriteEndObject();
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
+            writer.WritePropertyName("owningRelationship"u8);
+            if (iAnnotation.OwningRelationship.HasValue)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("@id"u8);
+                writer.WriteStringValue(iAnnotation.OwningRelationship.Value);
+                writer.WriteEndObject();
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
+            writer.WriteStartArray("source"u8);
+            foreach (var item in iAnnotation.Source)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("@id"u8);
+                writer.WriteStringValue(item);
+                writer.WriteEndObject();
+            }
+            writer.WriteEndArray();
+
+            writer.WriteStartArray("target"u8);
+            foreach (var item in iAnnotation.Target)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("@id"u8);
+                writer.WriteStringValue(item);
+                writer.WriteEndObject();
+            }
+            writer.WriteEndArray();
+
+            writer.WriteEndObject();
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
---
+++ KerML.NET.CodeGenerator.Tests/Expected/UML/AutoGenSerializer/CommentSerializer.cs
@@ -0,0 +1,121 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="CommentSerializer.cs" company="Starion Group S.A.">
+//
+//   Copyright 2022-2025 Starion Group S.A.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+// </copyright>
+// ------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
+
+namespace KerML.NET.Serializer.Json
+{
+    using System;
+    using System.Text.Json;
+
+    using SysML2.NET.Common;
+    using SysML2.NET.Core.DTO;
+    using SysML2.NET.Serializer.Json;
+
+    /// <summary>
+    /// The purpose of the <see cref="CommentSerializer"/> is to provide serialization capabilities
+    /// capabilities for the <see cref="IComment"/> interface
+    /// </summary>
+    public static class CommentSerializer
+    {
+        /// <summary>
+        /// Serializes an instance of <see cref="IComment"/> using an <see cref="Utf8JsonWriter"/>
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="IComment"/> to serialize
+        /// </param>
+        /// <param name="writer">
+        /// The target <see cref="Utf8JsonWriter"/>
+        /// </param>
+        /// <param name="serializationModeKind">
+        /// enumeration specifying what kind of serialization shall be used
+        /// </param>
+        /// <param name="inludeDerived">
+        /// A value indicating whether derived properties shall be serialized or not
+        /// </param>
+        public static void Serialize(object obj, Utf8JsonWriter writer, SerializationModeKind serializationModeKind, bool inludeDerived)
+        {
+            if (!(obj is IComment iComment))
+            {
+                throw new ArgumentException("The object shall be an IComment", nameof(obj));
+            }
+
+            writer.WriteStartObject();
+
+            writer.WritePropertyName("@type"u8);
+            writer.WriteStringValue("Comment"u8);
+
+            writer.WritePropertyName("@id"u8);
+            writer.WriteStringValue(iComment.Id);
+
+            writer.WriteStartArray("aliasIds"u8);
+            foreach (var item in iComment.AliasIds)
+            {
+                writer.WriteStringValue(item);
+            }
+            writer.WriteEndArray();
+
+            writer.WritePropertyName("body"u8);
+            writer.WriteStringValue(iComment.Body);
+
+            writer.WritePropertyName("declaredName"u8);
+            writer.WriteStringValue(iComment.DeclaredName);
+            writer.WritePropertyName("declaredShortName"u8);
+            writer.WriteStringValue(iComment.DeclaredShortName);
+            writer.WritePropertyName("elementId"u8);
+            writer.WriteStringValue(iComment.ElementId);
+
+            writer.WritePropertyName("isImpliedIncluded"u8);
+            writer.WriteBooleanValue(iComment.IsImpliedIncluded);
+
+            writer.WritePropertyName("locale"u8);
+            writer.WriteStringValue(iComment.Locale);
+            writer.WriteStartArray("ownedRelationship"u8);
+            foreach (var item in iComment.OwnedRelationship)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("@id"u8);
+                writer.WriteStringValue(item);
+                writer.WriteEndObject();
+            }
+            writer.WriteEndArray();
+
+            writer.WritePropertyName("owningRelationship"u8);
+            if (iComment.OwningRelationship.HasValue)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName("@id"u8);
+                writer.WriteStringValue(iComment.OwningRelationship.Value);
+                writer.WriteEndObject();
+            }
+            else
+            {
+                writer.WriteNullValue();
+            }
+            writer.WriteEndObject();
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// --------THIS IS AN AUTOMATICALLY GENERATED FILE. ANY MANUAL CHANGES WILL BE OVERWRITTEN!--------
+// ------------------------------------------------------------------------------------------------
